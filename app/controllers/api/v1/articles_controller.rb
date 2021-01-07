f34648c60343449aa69aeb6b733d01369320970f
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-01-07 10:18:31 -0300

    Finish CRUD and api

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,28 +2,45 @@ module Api
     module V1
         class ArticlesController < ApplicationController
 
-            #Listar todos os artigos
+            # Listar todos os artigos
             def index
                 articles = Article.order('created_at DESC');
                 render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }, status: :ok
             end
 
-            #Listar artigo passando o ID
+            # Listar artigo passando o ID
             def show
                 article = Article.find(params[:id])
                 render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
             end
 
-            #Criar um novo artigo
+            # Criar um novo artigo
             def create
                 article = Article.new(article_params)
                 if article.save
-                    render json: {status: 'SUCCESS', message: 'The Article has been saved', data: article}, status: :ok
+                    render json: { status: 'SUCCESS', message: 'The article has been saved', data: article }, status: :ok
                 else
-                    render json: {status: 'ERROR', message: "The Article couldn't be saved", data: article.error}, status: :unprocessable_entity
+                    render json: { status: 'ERROR', message: "The article couldn't be saved", data: article.error }, status: :unprocessable_entity
+                end
+            end
+
+            # Atualizar um artigo
+            def update
+                article = Article.find(params[:id])
+                if article.update_attributes(article_params)
+                    render json: { status: 'SUCCESS', message: 'The article has been updated', data: article }, status: :ok
+                else
+                    render json: { status: 'ERROR', message: "The article couldn't be updated", data: article.error }, status: :unprocessable_entity
                 end
             end
             
+            # Excluir um artigo
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: { status: 'SUCCESS', message: "The article has been deleted", data: article}, status: :ok
+            end
+
             # Parâmetros aceitos
             private
 
